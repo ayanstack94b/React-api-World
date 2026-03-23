04faef2e604fe9252182249ce5f809967ac61bb3
unknown <ayondevs.94b@gmail.com>
2026-03-23 18:46:44 +0530

    rest countries url added and fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Suspense } from "react"
+import Countries from "./Components/Countries/Countries"
 
-import './App.css'
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then((res)=> res.json())
+
 
 function App() {
 
 
   return (
     <>
-      
+      <h1>Hello react</h1>
+      <Suspense fallback={<p>Data Loading...</p>}>
+        <Countries countriesPromise={countriesPromise }></Countries>
+      </Suspense>
     </>
   )
 }
